70f848f9d2fbd4fe97cdfa1e654f31180525489d
Krishan <kmkrider1999@gmail.com>
2023-06-16 19:39:55 +0530

    Implemented CRUD

--- app.js
+++ app.js
@@ -33,6 +33,93 @@ app.get("/", async (req, res) => {
   res.send("hello");
 });
 
+// Making a add/post request
+app.post("/vendor", async (req, res) => {
+  const {
+    name,
+    accountNumber,
+    bankName,
+    address1,
+    address2,
+    city,
+    country,
+    zipcode,
+  } = req.body;
+
+  try {
+    const vendor = await Vendor.create({
+      name,
+      accountNumber,
+      bankName,
+      address1,
+      address2,
+      city,
+      country,
+      zipcode,
+    });
+    res.json(vendor);
+  } catch (error) {
+    console.log(error.message);
+    throw new Error();
+  }
+});
+
+// Making a get/fetch request on vendor
+app.get("/vendor", async (req, res) => {
+  res.json(await Vendor.find());
+});
+
+// Making a get/fetch request on vendor with the vendor's id
+app.get("/vendor/:id", async (req, res) => {
+  const { id } = req.params;
+  const vendorDoc = await Vendor.findById(id);
+  res.json(vendorDoc);
+  // vendorDoc.set({});
+});
+
+// Making a put/update request on vendor with vendor's id
+app.put("/vendor/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    accountNumber,
+    bankName,
+    address1,
+    address2,
+    city,
+    country,
+    zipcode,
+  } = req.body;
+  try {
+    const vendorDoc = await Vendor.findByIdAndUpdate(id, {
+      name,
+      accountNumber,
+      bankName,
+      address1,
+      address2,
+      city,
+      country,
+      zipcode,
+    });
+    vendorDoc.save();
+    res.json({ success: "success" });
+  } catch (error) {
+    console.log(error.message);
+    throw new Error();
+  }
+});
+
+// Making a delete/remove request on vendor with vendor's id
+app.delete("/vendor/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const vedorDeletion = await Vendor.findByIdAndDelete(id);
+    res.json({ success: "document deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 // Listening to the server
 app.listen(3001, () => console.log("listening"));
 
